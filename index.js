38a2886ece22149fee27669c20d4139d0663bfbc
Jonathan Cammack <jonathanrcammack@gmail.com>
2020-12-15 13:45:20 -0700

    adding trans # to each entry shown to facilitate changing data that already exists in DB

--- index.js
+++ index.js
@@ -142,7 +142,7 @@ express()
     })
   })
   .get('/get_user_info', function (req, res) {
-    var sql = 'SELECT cr.trans_date, cr.trans_location, ccl.cat_name, cr.amount, mcl.mop_type, cr.entry_desc, tcl.wd_type FROM users u INNER JOIN check_register_entry cr ON u.user_name = cr.user_name INNER JOIN cat_common_lookup ccl ON cr.category = ccl.cat_cl_id INNER JOIN mop_common_lookup mcl ON cr.pay_method = mcl.mop_cl_id INNER JOIN type_common_lookup tcl ON cr.wd_type = tcl.type_cl_id WHERE cr.user_name = $1;';
+    var sql = 'SELECT cr.entry_id, cr.trans_date, cr.trans_location, ccl.cat_name, cr.amount, mcl.mop_type, cr.entry_desc, tcl.wd_type FROM users u INNER JOIN check_register_entry cr ON u.user_name = cr.user_name INNER JOIN cat_common_lookup ccl ON cr.category = ccl.cat_cl_id INNER JOIN mop_common_lookup mcl ON cr.pay_method = mcl.mop_cl_id INNER JOIN type_common_lookup tcl ON cr.wd_type = tcl.type_cl_id WHERE cr.user_name = $1;';
     values = [req.session.username]
     pool.query(sql, values, function(err, result) {
       if (err) {
